e4d7b3b9a9c3c9cadf298591426c7f8162edaa17
Agomez2025379 <Agomez-2025379@kinal.edu.gt>
2026-08-28 01:40:25 -0600

    setup: scaffolds de producto-formulario, producto-listado y servicio inventario; shell de la app con rutas

---
+++ src/app/components/producto-formulario/producto-formulario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-producto-formulario',
+  styleUrl: './producto-formulario.css',
+  templateUrl: './producto-formulario.html',
+})
+export class ProductoFormulario {}
