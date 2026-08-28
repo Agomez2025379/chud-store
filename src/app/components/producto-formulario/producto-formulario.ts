3e9f8ca62d62074a5f2450ea2fd1afe9a2a4d97c
Agomez2025379 <Agomez-2025379@kinal.edu.gt>
2026-08-28 01:41:38 -0600

    form: plantilla HTML del formulario con [formGroup] y formControlName en cada campo

--- src/app/components/producto-formulario/producto-formulario.ts
+++ src/app/components/producto-formulario/producto-formulario.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-producto-formulario',
   styleUrl: './producto-formulario.css',
   templateUrl: './producto-formulario.html',
 })
-export class ProductoFormulario {}
+export class ProductoFormulario {
+  protected readonly formulario = new FormGroup({
+    codigo: new FormControl(''),
+    nombre: new FormControl(''),
+    categoria: new FormControl(''),
+    precioCompra: new FormControl(0),
+    precioVenta: new FormControl(0),
+    existencias: new FormControl(0),
+    stockMinimo: new FormControl(1),
+    proveedor: new FormControl(''),
+    fechaIngreso: new FormControl(''),
+    descripcion: new FormControl(''),
+    activo: new FormControl(true),
+  });
+}
